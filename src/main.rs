58ad27150d6fa644f18dd11c77aa7af95ed6894c
Iquiji <yt.failerbot.3000@gmail.com>
2022-12-02 15:40:11 +0100

    started on RSA and mod_pow should originally be ok -> dont understand problem yet

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,53 @@ fn main() {
     println!("Hello, world!");
     
     // RSA
-    const SIZE: usize = (4096) / 8;
-    let rsa_n_str = "bad47a84c1782e4dbdd913f2a261fc8b65838412c6e45a2068ed6d7f16e9cdf4462b39119563cafb74b9cbf25cfd544bdae23bff0ebe7f6441042b7e109b9a8afaa056821ef8efaab219d21d6763484785622d918d395a2a31f2ece8385a8131e5ff143314a82e21afd713bae817cc0ee3514d4839007ccb55d68409c97a18ab62fa6f9f89b3f94a2777c47d6136775a56a9a0127f682470bef831fbec4bcd7b5095a7823fd70745d37d1bf72b63c4b1b4a3d0581e74bf9ade93cc46148617553931a79d92e9e488ef47223ee6f6c061884b13c9065b591139de13c1ea2927491ed00fb793cd68f463f5f64baa53916b46c818ab99706557a1c2d50d232577d1";
+    const SIZE: usize = (64) / 8;
+    let rsa_n_str = "23 "; //"bad47a84c1782e4dbdd913f2a261fc8b65838412c6e45a2068ed6d7f16e9cdf4462b39119563cafb74b9cbf25cfd544bdae23bff0ebe7f6441042b7e109b9a8afaa056821ef8efaab219d21d6763484785622d918d395a2a31f2ece8385a8131e5ff143314a82e21afd713bae817cc0ee3514d4839007ccb55d68409c97a18ab62fa6f9f89b3f94a2777c47d6136775a56a9a0127f682470bef831fbec4bcd7b5095a7823fd70745d37d1bf72b63c4b1b4a3d0581e74bf9ade93cc46148617553931a79d92e9e488ef47223ee6f6c061884b13c9065b591139de13c1ea2927491ed00fb793cd68f463f5f64baa53916b46c818ab99706557a1c2d50d232577d1";
     let rsa_n = BigInt::<SIZE>::from_hex_string(rsa_n_str);
     println!("rsa_n: {:?}",rsa_n);
     let outstring = rsa_n.to_hex_string();
     // assert_eq!(rsa_n_str, outstring.to_lowercase());
 
-    let rsa_d_str = "40d60f24b61d76783d3bb1dc00b55f96a2a686f59b3750fdb15c40251c370c65cada222673811bc6b305ed7c90ffcb3abdddc8336612ff13b42a75cb7c88fb936291b523d80acce5a0842c724ed85a1393faf3d470bda8083fa84dc5f31499844f0c7c1e93fb1f734a5a29fb31a35c8a0822455f1c850a49e8629714ec6a2657efe75ec1ca6e62f9a3756c9b20b4855bdc9a3ab58c43d8af85b837a7fd15aa1149c119cfe960c05a9d4cea69c9fb6a897145674882bf57241d77c054dc4c94e8349d376296137eb421686159cb878d15d171eda8692834afc871988f203fc822c5dcee7f6c48df663ea3dc755e7dc06aebd41d05f1ca2891e2679783244d068f";
+    let rsa_d_str = "1D"; //"40d60f24b61d76783d3bb1dc00b55f96a2a686f59b3750fdb15c40251c370c65cada222673811bc6b305ed7c90ffcb3abdddc8336612ff13b42a75cb7c88fb936291b523d80acce5a0842c724ed85a1393faf3d470bda8083fa84dc5f31499844f0c7c1e93fb1f734a5a29fb31a35c8a0822455f1c850a49e8629714ec6a2657efe75ec1ca6e62f9a3756c9b20b4855bdc9a3ab58c43d8af85b837a7fd15aa1149c119cfe960c05a9d4cea69c9fb6a897145674882bf57241d77c054dc4c94e8349d376296137eb421686159cb878d15d171eda8692834afc871988f203fc822c5dcee7f6c48df663ea3dc755e7dc06aebd41d05f1ca2891e2679783244d068f";
     let rsa_d = BigInt::<SIZE>::from_hex_string(rsa_d_str);
+    println!("rsa_d: {:?}",rsa_d);
     let outstring = rsa_d.to_hex_string();
     // assert_eq!(rsa_d_str, outstring.to_lowercase());
 
-    let rsa_e_str = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010001";
+    let rsa_e_str = "05"; //"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010001";
     let rsa_e = BigInt::<SIZE>::from_hex_string(rsa_e_str);
+    println!("rsa_e: {:?}",rsa_e);
     let outstring = rsa_e.to_hex_string();
     // assert_eq!(rsa_e_str, outstring.to_lowercase());
 
-    let message = BigInt::<SIZE>::from_u8(42);
-    let mod_pow = message.mod_pow(rsa_d * rsa_e, rsa_n);
-    assert_eq!(message, mod_pow);
+    let message = BigInt::<SIZE>::from_hex_string("0C");
+    println!("message = {:?}",message);
+    let encrypted = message.mod_pow(rsa_e, rsa_n);
+    println!("mod_pow (c) = {:?}",encrypted);
+    
+    let decrypted = encrypted.mod_pow(rsa_d, rsa_n);
+    println!("decrypted = {:?}",decrypted);
+
+    assert_eq!(message, decrypted);
 
 
 
-    let number = BigInt::<4>::from_hex_string("01FEFE01");
-    assert_eq!(number.to_hex_string(),"01FEFE01");
-    let number1 = BigInt::<4>::from_hex_string("00_00_FF_FF");
-    let number2 = BigInt::<4>::from_hex_string("00_00_00_FF");
-    println!("Mul 1: {:?}", number1);
-    println!("Mul 2: {:?}", number2);
+    // let number = BigInt::<4>::from_hex_string("01FEFE01");
+    // assert_eq!(number.to_hex_string(),"01FEFE01");
+    // let number1 = BigInt::<4>::from_hex_string("00_00_FF_FF");
+    // let number2 = BigInt::<4>::from_hex_string("00_00_00_FF");
+    // println!("Mul 1: {:?}", number1);
+    // println!("Mul 2: {:?}", number2);
     // println!("Debug: {:?}", number2.shl(15));
     // let mut accum = BigInt::<4>::new();
     // for i in 0..16{
     //     accum = accum + (number2.shl(i));
     // }
-    println!("Expected: {:?}", number);
+    // println!("Expected: {:?}", number);
 
     // println!("Manual: {:?}", accum);
 
-    println!("Debug: {:?}", number1 / number2);
+    // println!("Debug: {:?}", number1 / number2);
     // println!("Debug: {:?}", number2);
     // println!("Debug: {:?}", number2.shl(0));
 
@@ -67,6 +75,34 @@ fn main() {
     // println!("Debug: {:?}", double);
     // let number3 = number - number2;
     // println!("Debug: {:?}", number3);
+}
+
+struct RSA<const SIZE: usize>{
+    p: BigInt::<SIZE>,
+    q: BigInt::<SIZE>,
+    n: BigInt::<SIZE>,
+    e: BigInt::<SIZE>,
+    d: BigInt::<SIZE>,
+}
+impl<const SIZE: usize> RSA<{ SIZE }>{
+    fn generate() -> Self{
+        unimplemented!()
+    }
+    fn from_n_d_e() -> Self{
+        unimplemented!()
+    }
+    fn get_public_key(&self) -> (BigInt::<SIZE>,BigInt::<SIZE>){
+        (self.e, self.n)
+    }
+    fn get_private_key(&self) -> (BigInt::<SIZE>,BigInt::<SIZE>){
+        (self.d, self.n)
+    }
+    fn encrypt(&self, data: BigInt::<SIZE>) -> BigInt::<SIZE>{
+        unimplemented!()
+    }
+    fn decrypt(&self, data: BigInt::<SIZE>) -> BigInt::<SIZE>{
+        unimplemented!()
+    }
 }
 
 const ELEMENT_BIT_SIZE: usize = 8;
@@ -407,7 +443,7 @@ impl<const SIZE: usize> BigInt<{ SIZE }>{
             iter += 1;
             // println!("exp: {:?}",exp);
             // println!("res: {:?}",result);
-            if (exp.data[0] & 1) == 1 {
+            if exp % BigInt::from_u8(2) == 1.into() {
                 result = (result * base) % modulus;
             }
             base = (base * base) % modulus;
